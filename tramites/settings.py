294d3d2c799b4553f704fe2cf8f45e9ce25d1459
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-05-29 18:09:54 -0300

    Se realizaron cambios basicos para publicar.

--- tramites/settings.py
+++ tramites/settings.py
@@ -138,8 +138,8 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mmandrille@gmail.com'
-EMAIL_HOST_PASSWORD = 'comoyoquiera15'
+EMAIL_HOST_USER = 'yourmail'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 
 #Actualizar Statics, no solo nuevas
 AWS_PRELOAD_METADATA = True
