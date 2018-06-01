55acc07989e4f66eb0240b5079363151ab26361d
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-06-01 19:15:40 -0300

    Se hicieron cambios basicos

--- core/admin.py
+++ core/admin.py
@@ -3,15 +3,18 @@ from django.contrib import admin
 from .models import Tramite, Guia, Archivo
 
 #Definimos Inline
-#Definimos inline
+class TramiteAdmin(admin.ModelAdmin):
+    search_fields = ['nombre', 'organismo']
+
 class ArchivoInline(admin.TabularInline):
     model = Archivo
 
 class GuiaAdmin(admin.ModelAdmin):
+    search_fields = ['nombre', 'organismo']
     inlines = [
         ArchivoInline,
     ]
 
 # Register your models here.
-admin.site.register(Tramite)
+admin.site.register(Tramite, TramiteAdmin)
 admin.site.register(Guia, GuiaAdmin)
